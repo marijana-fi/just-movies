5e9398b021f0c18f4429bece33bf6b42d2e33fd5
marijana-fi <61113052+marijana-fi@users.noreply.github.com>
2020-03-22 17:48:45 +0100

    add .env and outline styles

--- src/ApiData.js
+++ src/ApiData.js
@@ -1,52 +1,64 @@
 const axios = require("axios");
+const key = process.env.REACT_APP_API_KEY;
 
 const BASE = "https://api.themoviedb.org/3/";
-const API_KEY = "?api_key=YOUR_API_KEY";
+const API_KEY = `?api_key=${key}`;
 
 const success = ({ data }) => data;
 
 const fail = err => {
-   switch (err.status) {
-      case 400:
-         console.error("API error: invalid parameters");
-         break;
-      case 401:
-         console.error("API error: insufficient credentials");
-         break;
-      case 403:
-         console.error("API error: insufficient access");
-         break;
-      case 404:
-         console.error("API error: endpoint not found");
-         break;
-      default:
-         console.error("Something went wrong");
-   }
-   return err;
+	switch (err.status) {
+		case 400:
+			console.error("API error: invalid parameters");
+			break;
+		case 401:
+			console.error("API error: insufficient credentials");
+			break;
+		case 403:
+			console.error("API error: insufficient access");
+			break;
+		case 404:
+			console.error("API error: endpoint not found");
+			break;
+		default:
+			console.error("Something went wrong");
+	}
+	return err;
 };
 
 const makeRequest = (path, query = "", page = "", sort = "") =>
-   axios.get(`${BASE}${path}${API_KEY}${query}${sort}${page}&region=US`).then(success, fail);
+	axios
+		.get(`${BASE}${path}${API_KEY}${query}${sort}${page}&region=US`)
+		.then(success, fail);
 
 const api = {
-   movies(path) {
-      return makeRequest(path);
-   },
-   search(q) {
-      return makeRequest("search/movie", `&query=${q}`);
-   },
-   loadMore(path, searchQuery, page, genreId) {
-      return makeRequest(path, `&query=${searchQuery}&with_genres=${genreId}`, page);
-   },
-   movie(path) {
-      return makeRequest(path, `&append_to_response=videos,credits`);
-   },
-   genreList() {
-      return makeRequest("genre/movie/list");
-   },
-   moviesByGenre(id, sortValue = "popularity.desc") {
-      return makeRequest("discover/movie", `&with_genres=${id}`, "", `&sort_by=${sortValue}`);
-   }
+	movies(path) {
+		return makeRequest(path);
+	},
+	search(q) {
+		return makeRequest("search/movie", `&query=${q}`);
+	},
+	loadMore(path, searchQuery, page, genreId) {
+		return makeRequest(
+			path,
+			`&query=${searchQuery}&with_genres=${genreId}`,
+			page
+		);
+	},
+	movie(path) {
+		return makeRequest(path, `&append_to_response=videos,credits`);
+	},
+	genreList() {
+		return makeRequest("genre/movie/list");
+	},
+	moviesByGenre(id, sortValue = "popularity.desc") {
+		return makeRequest(
+			"discover/movie",
+			`&with_genres=${id}`,
+			"",
+			`&sort_by=${sortValue}`
+		);
+	}
 };
 
 export default api;
--- src/App.js
+++ src/App.js
@@ -12,207 +12,216 @@ import { Provider } from "./components/PageContext";
 import GenreResults from "./components/genre-results/GenreResults";
 
 class App extends React.Component {
-   constructor(props) {
-      super(props);
-      this.state = {
-         loading: true,
-         isMenuVisible: false,
-         movies: [],
-         favorites: [],
-         genreList: [],
-         totalPages: null,
-         activePath: "",
-         activeGenre: "",
-         activeGenreName: "",
-         page: 1,
-         searchQuery: "",
-         handleLoadMore: this.handleLoadMore,
-         stopLoading: this.stopLoading,
-         sortBy: { label: "Popularity", value: "popularity.desc" }
-      };
-   }
+	constructor(props) {
+		super(props);
+		this.state = {
+			loading: true,
+			isMenuVisible: false,
+			movies: [],
+			favorites: [],
+			genreList: [],
+			totalPages: null,
+			activePath: "",
+			activeGenre: "",
+			activeGenreName: "",
+			page: 1,
+			searchQuery: "",
+			handleLoadMore: this.handleLoadMore,
+			stopLoading: this.stopLoading,
+			sortBy: { label: "Popularity", value: "popularity.desc" }
+		};
+	}
 
-   componentDidMount() {
-      this.loadFavoritesFromLocalStorage();
-   }
+	componentDidMount() {
+		this.loadFavoritesFromLocalStorage();
+	}
 
-   stopLoading = () => {
-      this.setState({ loading: false });
-   };
+	stopLoading = () => {
+		this.setState({ loading: false });
+	};
 
-   toggleMenu = () => {
-      this.setState({ isMenuVisible: !this.state.isMenuVisible });
-   };
+	toggleMenu = () => {
+		this.setState({ isMenuVisible: !this.state.isMenuVisible });
+	};
 
-   addToFavorites = movie => {
-      this.setState({ favorites: [...this.state.favorites, movie] }, () => {
-         localStorage.setItem("movies", JSON.stringify(this.state.favorites));
-      });
+	addToFavorites = movie => {
+		this.setState({ favorites: [...this.state.favorites, movie] }, () => {
+			localStorage.setItem(
+				"movies",
+				JSON.stringify(this.state.favorites)
+			);
+		});
 
-      if (this.state.favorites.some(item => item.id === movie.id)) {
-         this.removeFromFavorites(movie);
-      }
-   };
+		if (this.state.favorites.some(item => item.id === movie.id)) {
+			this.removeFromFavorites(movie);
+		}
+	};
 
-   removeFromFavorites = movie => {
-      this.setState({
-         favorites: this.state.favorites.filter(item => item.id !== movie.id)
-      });
-   };
+	removeFromFavorites = movie => {
+		this.setState({
+			favorites: this.state.favorites.filter(item => item.id !== movie.id)
+		});
+	};
 
-   loadFavoritesFromLocalStorage = () => {
-      const favorites = JSON.parse(localStorage.getItem("movies"));
-      if (favorites) {
-         this.setState({
-            favorites: favorites,
-            loading: false
-         });
-      }
-   };
+	loadFavoritesFromLocalStorage = () => {
+		const favorites = JSON.parse(localStorage.getItem("movies"));
+		if (favorites) {
+			this.setState({
+				favorites: favorites,
+				loading: false
+			});
+		}
+	};
 
-   fetchMovies = path => {
-      api.movies(path).then(data =>
-         this.setState({
-            movies: data.results,
-            page: 1,
-            totalPages: data.total_pages,
-            activePath: path,
-            singleMovie: {},
-            loading: false
-         })
-      );
-      window.scrollTo(0, 0);
-   };
+	fetchMovies = path => {
+		api.movies(path).then(data =>
+			this.setState({
+				movies: data.results,
+				page: 1,
+				totalPages: data.total_pages,
+				activePath: path,
+				singleMovie: {},
+				loading: false
+			})
+		);
+		window.scrollTo(0, 0);
+	};
 
-   searchMovies = q => {
-      api.search(q).then(data =>
-         this.setState({
-            movies: data.results,
-            page: 1,
-            totalPages: data.total_pages,
-            activePath: "search/movie"
-         })
-      );
-   };
+	searchMovies = q => {
+		api.search(q).then(data =>
+			this.setState({
+				movies: data.results,
+				page: 1,
+				totalPages: data.total_pages,
+				activePath: "search/movie"
+			})
+		);
+	};
 
-   getGenre = (id, label, sortValue) => {
-      api.moviesByGenre(id, sortValue).then(data => {
-         console.log(sortValue);
-         this.setState({
-            movies: data.results,
-            activePath: "discover/movie",
-            activeGenre: parseInt(id),
-            activeGenreName: label,
-            loading: false
-         });
-      });
-   };
+	getGenre = (id, label, sortValue) => {
+		api.moviesByGenre(id, sortValue).then(data => {
+			this.setState({
+				movies: data.results,
+				activePath: "discover/movie",
+				activeGenre: parseInt(id),
+				activeGenreName: label,
+				loading: false
+			});
+		});
+	};
 
-   updateSearchValue = value => {
-      this.setState({ searchQuery: value });
-   };
+	updateSearchValue = value => {
+		this.setState({ searchQuery: value });
+	};
 
-   resetGenreName = () => {
-      this.setState({
-         activeGenreName: "Select genre",
-         activeGenre: ""
-      });
-   };
+	resetGenreName = () => {
+		this.setState({
+			activeGenreName: "Select genre",
+			activeGenre: ""
+		});
+	};
 
-   setSort = sortValue => {
-      this.setState({ sortBy: sortValue });
-   };
+	setSort = sortValue => {
+		this.setState({ sortBy: sortValue });
+	};
 
-   setActivePath = activePath => {
-      this.setState({ activePath });
-   };
+	setActivePath = activePath => {
+		this.setState({ activePath });
+	};
 
-   handleLoadMore = () => {
-      const { page, totalPages } = this.state;
+	handleLoadMore = () => {
+		const { page, totalPages } = this.state;
 
-      this.setState(
-         {
-            page: page === totalPages ? totalPages : page + 1
-         },
-         () => {
-            const page = `&page=${this.state.page}`;
-            const { activePath, searchQuery, activeGenre } = this.state;
+		this.setState(
+			{
+				page: page === totalPages ? totalPages : page + 1
+			},
+			() => {
+				const page = `&page=${this.state.page}`;
+				const { activePath, searchQuery, activeGenre } = this.state;
 
-            api.loadMore(activePath, searchQuery, page, activeGenre).then(data =>
-               this.setState({
-                  movies: [...this.state.movies, ...data.results]
-               })
-            );
-         }
-      );
-   };
+				api.loadMore(activePath, searchQuery, page, activeGenre).then(
+					data =>
+						this.setState({
+							movies: [...this.state.movies, ...data.results]
+						})
+				);
+			}
+		);
+	};
 
-   render() {
-      return (
-         <div className="App">
-            <SideBar
-               fetchMovies={this.fetchMovies}
-               data={this.state}
-               displayFavorites={this.displayFavorites}
-               isMenuVisible={this.state.isMenuVisible}
-               toggleMenu={this.toggleMenu}
-               getGenre={this.getGenre}
-               setActivePath={this.setActivePath}
-               setSort={this.setSort}
-            />
-            <Header
-               isMenuVisible={this.state.isMenuVisible}
-               searchQuery={this.state.searchQuery}
-               searchMovies={this.searchMovies}
-               updateSearchValue={this.updateSearchValue}
-               toggleMenu={this.toggleMenu}
-               resetGenreName={this.resetGenreName}
-            />
-            <Provider value={this.state}>
-               <Router>
-                  <Home
-                     path="/"
-                     data={this.state}
-                     handleLoadMore={this.handleLoadMore}
-                     fetchMovies={this.fetchMovies}
-                     resetGenreName={this.resetGenreName}
-                  />
-                  <MovieDetails
-                     path="/movie/:id"
-                     movieData={this.state}
-                     loading={this.state.loading}
-                     addToFavorites={this.addToFavorites}
-                     getGenre={this.getGenre}
-                     resetGenreName={this.resetGenreName}
-                     setSort={this.setSort}
-                  />
-                  <Home
-                     path="/:activePath"
-                     data={this.state}
-                     handleLoadMore={this.handleLoadMore}
-                     fetchMovies={this.fetchMovies}
-                     resetGenreName={this.resetGenreName}
-                  />
-                  <GenreResults
-                     path="/genres/:id"
-                     data={this.state}
-                     handleLoadMore={this.handleLoadMore}
-                     getGenre={this.getGenre}
-                     setSort={this.setSort}
-                  />
-                  <SearchResults path="/search" data={this.state} handleLoadMore={this.handleLoadMore} />
-                  <Favorites
-                     path="/favorites"
-                     data={this.state}
-                     loadFavoritesFromLocalStorage={this.loadFavoritesFromLocalStorage}
-                     resetGenreName={this.resetGenreName}
-                     setActivePath={this.setActivePath}
-                  />
-               </Router>
-            </Provider>
-         </div>
-      );
-   }
+	render() {
+		return (
+			<div className="App">
+				<SideBar
+					fetchMovies={this.fetchMovies}
+					data={this.state}
+					displayFavorites={this.displayFavorites}
+					isMenuVisible={this.state.isMenuVisible}
+					toggleMenu={this.toggleMenu}
+					getGenre={this.getGenre}
+					setActivePath={this.setActivePath}
+					setSort={this.setSort}
+				/>
+				<Header
+					isMenuVisible={this.state.isMenuVisible}
+					searchQuery={this.state.searchQuery}
+					searchMovies={this.searchMovies}
+					updateSearchValue={this.updateSearchValue}
+					toggleMenu={this.toggleMenu}
+					resetGenreName={this.resetGenreName}
+				/>
+				<Provider value={this.state}>
+					<Router>
+						<Home
+							path="/"
+							data={this.state}
+							handleLoadMore={this.handleLoadMore}
+							fetchMovies={this.fetchMovies}
+							resetGenreName={this.resetGenreName}
+						/>
+						<MovieDetails
+							path="/movie/:id"
+							movieData={this.state}
+							loading={this.state.loading}
+							addToFavorites={this.addToFavorites}
+							getGenre={this.getGenre}
+							resetGenreName={this.resetGenreName}
+							setSort={this.setSort}
+						/>
+						<Home
+							path="/:activePath"
+							data={this.state}
+							handleLoadMore={this.handleLoadMore}
+							fetchMovies={this.fetchMovies}
+							resetGenreName={this.resetGenreName}
+						/>
+						<GenreResults
+							path="/genres/:id"
+							data={this.state}
+							handleLoadMore={this.handleLoadMore}
+							getGenre={this.getGenre}
+							setSort={this.setSort}
+						/>
+						<SearchResults
+							path="/search"
+							data={this.state}
+							handleLoadMore={this.handleLoadMore}
+						/>
+						<Favorites
+							path="/favorites"
+							data={this.state}
+							loadFavoritesFromLocalStorage={
+								this.loadFavoritesFromLocalStorage
+							}
+							resetGenreName={this.resetGenreName}
+							setActivePath={this.setActivePath}
+						/>
+					</Router>
+				</Provider>
+			</div>
+		);
+	}
 }
 
 export default App;
